7deea04abaa4cc6dd229d7c75f9683f2bb06f20e
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-03-19 17:35:43 +0000

    submit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Movies-Library
+# Movies-Library - 1.0
+
+
+**Author Name**: ibraheem areeda
+
+## WRRC
+![](./WRRC.PNG)
+
+## Overview
+
+## Getting Started
+Assuming you’ve already installed Node.js, create a directory to hold your application, and make that your working directory.
+1- $ mkdir myapp
+   $ cd myapp
+
+2- $ npm init
+
+3- $ npm install express
+
+4- $ node server.js
+
+
+## Project Features
+
+http://localhost:3004/favorite => Welcome to Favorite Page
+
+
+http://localhost:3004/ => 
+
+{
+  "title": "Spider-Man: No Way Home",
+  "poster_path": "/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+  "overview": "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man."
+}
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express()
+const port = 3004
+const data = require("./data/data.json")
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
+function movie(title, genre_ids, original_language, original_title,
+    poster_path, video, vote_average, overview, release_date, vote_count,
+    id, adult, backdrop_path, popularity, media_type) {
+
+    this.title = title;
+    this.genre_ids = genre_ids;
+    this.original_language = original_language;
+    this.original_title = original_title;
+    this.poster_path = poster_path;
+    this.video = video;
+    this.vote_average = vote_average;
+    this.overview = overview;
+    this.release_date = release_date;
+    this.vote_count = vote_count;
+    this.id = id;
+    this.adult = adult;
+    this.backdrop_path = backdrop_path;
+    this.popularity = popularity;
+    this.media_type = media_type;
+}
+
+
+
+
+
+
+
+
+
+const fhandler = (req, res) => {
+    res.send('Welcome to Favorite Page')
+}
+
+
+const homeHandler = (req , res) => {
+    const allPropMovie = new  movie (data.title, data.genre_ids, data.original_language, data.original_title,
+    data.poster_path, data.video, data.vote_average, data.overview, data.release_date, data.vote_count,
+    data.id, data.adult, data.backdrop_path, data.popularity, data.media_type);
+
+    const reqPropMovie = {}
+    reqPropMovie.title = allPropMovie.title
+    reqPropMovie.poster_path = allPropMovie.poster_path
+    reqPropMovie.overview = allPropMovie.overview
+
+
+    res.json(reqPropMovie)
+}
+
+
+app.get ('/favorite', fhandler)
+
+
+app.get('/', homeHandler)
+
+
+
